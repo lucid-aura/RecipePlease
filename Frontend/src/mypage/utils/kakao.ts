fc1575df53ed7646a3e57de5ffb291549ad5176d
nogal2 <shtmdgus@gmail.com>
2022-03-30 11:27:20 +0900

    카카오 로그인후 프로필 조회 구현

--- Frontend/src/mypage/utils/kakao.ts
+++ Frontend/src/mypage/utils/kakao.ts
@@ -1,15 +1,11 @@
 import { KakaoOAuthToken, KakaoProfile, login, logout, getProfile as getKakaoProfile, unlink } from "@react-native-seoul/kakao-login";
-import { useNavigation } from "@react-navigation/native";
 
-const navigation = useNavigation()
 // 로그인
 export const signInWithKakao = async (): Promise<void> => {
     const token: KakaoOAuthToken = await login();
 
-    if(token != null || token != undefined) {
-        
-        console.log(JSON.stringify(token))
-    }
+    console.log(JSON.stringify(token))
+    
 };
 
 // 로그아웃
@@ -19,11 +15,10 @@ export const signOutWithKakao = async (): Promise<void> => {
 };
 
 // 프로필 조회
-export const getProfile = async (): Promise<void> => {
+export const getProfile = async (): Promise<string> => {
     const profile: KakaoProfile = await getKakaoProfile();
-
-    console.log(JSON.stringify(profile))
-
+    JSON.stringify(profile.id)
+    return profile.id
 };
 
 export const unlinkKakao = async (): Promise<void> => {
