42119da29ef86a10384dbb24389f7ea72f2a41c6
lucid-aura <lucidaura@daum.net>
2022-04-17 00:20:23 +0900

    temp
    temp

--- Backend/src/main/java/com/recipe/a/controller/GoodsController.java
+++ Backend/src/main/java/com/recipe/a/controller/GoodsController.java
@@ -1,5 +1,7 @@
 package com.recipe.a.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,9 +22,16 @@ public class GoodsController {
 		System.out.println("GoodsController countGoods()");
 		return "asd";
 	}
+	
 	@RequestMapping(value = "/goodsData", method = {RequestMethod.GET, RequestMethod.POST})
 	public GoodsDto goodsData() {
 		System.out.println("GoodsController goodsData()");
 		return goodsService.goodsData();
+	}
+	
+	@RequestMapping(value = "/getGoodsByCategory", method = {RequestMethod.GET, RequestMethod.POST})
+	public List<GoodsDto> getGoodsByCategory(String category) {
+		System.out.println("GoodsController getGoodsByCategory()");
+		return goodsService.getGoodsByCategory(category);
 	}
 }
--- Backend/src/main/java/com/recipe/a/dao/GoodsDao.java
+++ Backend/src/main/java/com/recipe/a/dao/GoodsDao.java
@@ -14,4 +14,6 @@ public interface GoodsDao {
 	public int countGoods();
 
 	public GoodsDto goodsData();
+	
+	public List<GoodsDto> getGoodsByCategory(String category);
 }
